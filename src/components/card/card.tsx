4ff16941ef6b1645582b9135a785f58e117d6455
acarlie <ameliacarlie@gmail.com>
2023-01-17 09:52:10 -0500

    refactor(card): refactoring use of emotion to decrease <style> tags on render

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,90 +1,100 @@
 /** @jsxImportSource @emotion/react */
 
-
 import { css, keyframes } from "@emotion/react"
+import CSS from 'csstype';
 
 import { GlowAnimationProps, SideProps, CornerProps, CardProps } from "./card.definitions"
 
-const glow = keyframes`
-    0%, 100% {
-        filter: drop-shadow(0px 0px 3px rgba(0, 255, 255, .5));
-    }
-    50% {
-        filter: drop-shadow(0px 0px 5px rgba(0, 255, 255, .75));
-    }
-`
-
-const fill = css`
-    opacity: 0.2;
-    fill: aqua;
-`
-
-const content = css`
-    background: aqua;
-    opacity: 0.2;
-`
-
-const topStroke = css`
-    stroke: #c5fffd;
-    stroke-width: 2px;
-`
-
-const bottomStroke = css`
-    stroke: aqua;
-    stroke-width: 3px;
-`
-
-const stroke = css`
-    fill: none;
-    stroke-miterlimit: 10;
-    filter: drop-shadow(0px 0px 3px rgba(0, 255, 255, .5));
-`
-
-const animate = ({ length, delay }: GlowAnimationProps) => css`
-    animation: ${glow} ${length}ms linear ${delay}ms infinite;
-`
-
-const container = css`
-    overflow: visible;
-`
 
 const cornerSize = '22px'
 
-const grid = css`
-    display: grid;
-    grid-template-columns: ${cornerSize} minmax(0px, auto) ${cornerSize};
-    grid-template-rows: ${cornerSize} minmax(0px, auto) ${cornerSize};
-    height: 100%;
-    width: 100%;
-`
+const animations = {
+    glow: keyframes`
+        0%, 100% {
+            filter: drop-shadow(0px 0px 3px rgba(0, 255, 255, .5));
+        }
+        50% {
+            filter: drop-shadow(0px 0px 5px rgba(0, 255, 255, .75));
+        }
+    `
+}
 
+const cardStyles = {
+    content: css`
+        background: aqua;
+        opacity: 0.2;
+    `,
+    grid: css`
+        display: grid;
+        grid-template-columns: ${cornerSize} minmax(0px, auto) ${cornerSize};
+        grid-template-rows: ${cornerSize} minmax(0px, auto) ${cornerSize};
+        height: 100%;
+        width: 100%;
+
+        .container {
+            overflow: visible;
+            
+            .fill {
+                opacity: 0.2;
+                fill: aqua;
+            }
+
+            [class^="stroke"] {
+                fill: none;
+                stroke-miterlimit: 10;
+                filter: drop-shadow(0px 0px 3px rgba(0, 255, 255, .5));
+                animation-name: ${animations.glow};
+            }
+
+            .stroke-top {
+                stroke: #c5fffd;
+                stroke-width: 2px;
+            }
+
+            .stroke-bottom {
+                stroke: aqua;
+                stroke-width: 3px;
+            }
+        }
+    `
+}
+
+
+const animate = ({ length, delay }: GlowAnimationProps) => {
+    const ret: CSS.Properties = {
+        animationDuration: `${length}ms`,
+        animationDelay: `${delay}ms`,
+        animationIterationCount: 'infinite',
+        animationTimingFunction: 'linear'
+    }
+    return ret
+}
 
 const Corner = ({ rotation, animation, isClipped }: CornerProps) => {
     const path = isClipped ? "M22,2H18l-16,16v4" : "M22,2H2,2v20"
 
     return (
-        <svg css={container} style={{ transform: `rotate(${rotation}deg)` }} xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 22 22">
-            <path css={fill} d={`${path}H22Z`} />
-            <path css={[stroke, animation && animate(animation), bottomStroke]} d={path} />
-            <path css={[stroke, animation && animate(animation), topStroke]} d={path} />
+        <svg className="container" style={{ transform: `rotate(${rotation}deg)` }} xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 22 22">
+            <path className="fill" d={`${path}H22Z`} />
+            <path className="stroke-bottom" d={path} style={animation && animate(animation)} />
+            <path className="stroke-top" d={path} style={animation && animate(animation)} />
         </svg>
     )
 }
 
-
 const SideHorizontal = ({ rotation, animation }: SideProps) => (
-    <svg css={container} style={{ transform: `rotate(${rotation}deg)` }} xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 60 22" preserveAspectRatio="none">
-        <rect css={fill} y="2" width="60" height="20" />
-        <line css={[stroke, animation && animate(animation), bottomStroke]} x1="60" y1="2" y2="2" />
-        <line css={[stroke, animation && animate(animation), topStroke]} x1="60" y1="2" y2="2" />
+    <svg className="container" style={{ transform: `rotate(${rotation}deg)` }} xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 60 22" preserveAspectRatio="none">
+        <rect className="fill" y="2" width="60" height="20" />
+        <line className="stroke-bottom" x1="60" y1="2" y2="2" style={animation && animate(animation)} />
+        <line className="stroke-top" x1="60" y1="2" y2="2" style={animation && animate(animation)} />
     </svg>
 )
 
 const SideVertical = ({ rotation, animation }: SideProps) => (
-    <svg css={container} style={{ transform: `rotate(${rotation}deg)` }} xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 22 60" preserveAspectRatio="none">
-        <rect css={fill} x="2" width="20" height="60" />
-        <line css={[stroke, animation && animate(animation), bottomStroke]} x1="2" x2="2" y2="60" />
-        <line css={[stroke, animation && animate(animation), topStroke]} x1="2" x2="2" y2="60" />
+    <svg className="container" style={{ transform: `rotate(${rotation}deg)` }} xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 22 60" preserveAspectRatio="none">
+        <rect className="fill" x="2" width="20" height="60" />
+        <line className="stroke-bottom" x1="2" x2="2" y2="60" style={animation && animate(animation)} />
+        <line className="stroke-top" x1="2" x2="2" y2="60" style={animation && animate(animation)} />
     </svg>
 )
 
@@ -92,7 +102,7 @@ const SideVertical = ({ rotation, animation }: SideProps) => (
 export const Card = ({ tl, tr, bl, br, children, animation }: CardProps) => {
 
     return (
-        <div css={grid}>
+        <div css={cardStyles.grid}>
 
             {/* Top */}
             <Corner rotation={0} isClipped={tl} animation={animation} />
@@ -101,7 +111,7 @@ export const Card = ({ tl, tr, bl, br, children, animation }: CardProps) => {
 
             {/* Center */}
             <SideVertical rotation={0} animation={animation} />
-            <div css={content}>
+            <div css={cardStyles.content}>
                 {children}
             </div>
             <SideVertical rotation={180} animation={animation} />
